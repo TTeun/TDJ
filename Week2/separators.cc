df658f3fcbf70bb882950d7dc6e17a1b357fa603
Dani3981 <danihulzebos@hotmail.com>
2016-09-17 16:12:50 +0200

    Update separators.cc

--- Week2/separators.cc
+++ Week2/separators.cc
@@ -8,10 +8,12 @@ long long value;
 
 int main()
 {
-cout << "Please enter a number which needs separators: \n > ";
-cin >> value;
-cout << "Your value with separators is ";
-printBig(value);
-cout << endl;
-printBigTwo(value);
+  cout << "Please enter a number which needs separators: \n > ";
+  cin >> value;
+  cout << "Your value with separators is ";
+  printBig(value);                              //using recursive function
+  cout << endl;
+  cout << "Your value with separators is ";
+  printBigTwo(value);                           //using other function
+  cout << endl;
 }
