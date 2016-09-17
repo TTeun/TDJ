fbb13a37d64389f90b4bd7435500da5920d016de
Dani3981 <danihulzebos@hotmail.com>
2016-09-17 12:04:30 +0200

    Update angletime.cc

--- week1/angletime.cc
+++ week1/angletime.cc
@@ -18,13 +18,13 @@ int main()
     if (input == 0)
       exit(0);
 
-    rounded = round(input / 15) * 15;
-    xx = floor(rounded / 30);
-    yy = xx == (rounded / 30) ? "00" : "30";
-    xx = xx % 12;
+    rounded = round(input / 15) * 15;                  //round to nearest 15 degrees
+    xx = floor(rounded / 30);                          //compute the hours
+    yy = xx == (rounded / 30) ? "00" : "30";           //compute the minutes
+    xx = xx % 12;                                      //make sure xx is between 1 and 12
     if (xx == 0)
       xx = 12;
-
+                                                       //display time
     cout << "object at your " << xx << ":" << yy << " position" << "\n";
   }
 }
