6c64452359153f9e1db5e1f88ce19bbaa5947782
TTeun <teunverstraaten@gmail.com>
2016-09-21 22:23:06 +0200

    asdsa

--- week2/dq/showresults.cc
+++ week2/dq/showresults.cc
@@ -1,12 +1,12 @@
 int findPercentage(variables& var) {
-	int value = 200 * (var.docCounter / static_cast<float>(var.lineCounter));
-	return value > 100 ? 100 : value;
+    int value = 200 * (var.docCounter / static_cast<float>(var.lineCounter));
+    return value > 100 ? 100 : value;
 }
 
 void showResults(variables& var) {
-	cout << "Total line Weight: " << var.lineCounter << '\n';
-	cout << "Number of eoln comments: " << var.eolnComments << '\n';
-	cout << "Number of C-style comments: " << var.cComments << '\n';
-	cout << "Maximum nesting depth: " << var.maxNest << '\n';
-	cout << "Documentation quality: " << findPercentage(var) << "\%\n";
+    cout << "Total line Weight: " << var.lineCounter << '\n';
+    cout << "Number of eoln comments: " << var.eolnComments << '\n';
+    cout << "Number of C-style comments: " << var.cComments << '\n';
+    cout << "Maximum nesting depth: " << var.maxNest << '\n';
+    cout << "Documentation quality: " << findPercentage(var) << "\%\n";
 }
