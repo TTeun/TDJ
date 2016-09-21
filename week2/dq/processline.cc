fb030369e123014032031faa904a2c3fbbac8765
TTeun <teunverstraaten@gmail.com>
2016-09-21 17:01:29 +0200

    sblablabla

--- week2/dq/processline.cc
+++ week2/dq/processline.cc
@@ -1,5 +1,5 @@
 void processLine(string& inputLine, variables& var) {
-	processCComments(inputLine, var);  // process C-style comments 
+	processCComments(inputLine, var);  // process C-style comments
 	setWeights(inputLine, var);        // Adjust the nesting and line weights
 	checkEOLNComments(inputLine, var); // update line and eoln counter
 }
