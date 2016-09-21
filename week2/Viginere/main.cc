bb5425db9f63058d3e77df1c0cc94039866b2381
Dani3981 <danihulzebos@hotmail.com>
2016-09-21 21:28:57 +0200

    Update main.cc

--- week2/Viginere/main.cc
+++ week2/Viginere/main.cc
@@ -2,13 +2,12 @@
 
 int main(int argc, char **argv)
 {
-  //cout << argv[1];  
   Vars vars;
   vars = arguments(cin, argc, argv[1]);
   if (vars.action == USAGE)
   {
     cerr << "Please use only 1 (for encrypting) or 2 (for decrypting) command line arguments.\n";
-    //exit(0);
+    exit(0);
   }
   char* text;
   text = vigenere(vars);
