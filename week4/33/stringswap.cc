54ff909107719719b08c1aee3d47f86fb400d78f
Dani3981 <danihulzebos@hotmail.com>
2016-10-11 13:36:14 +0200

    sadkjfa

--- week4/33/stringswap.cc
+++ week4/33/stringswap.cc
@@ -1,23 +1,21 @@
 #include <iostream>
+#include "strings/strings.h"
 
 using namespace std;
 
-void stringSwap(string str1, string str2);
-
-int main(){
-
-	string *str1 = "string1";
-	string *str2 = "string2";
-	
-	void stringSwap(string *str1, string *str2)
-	{
-		string *tmp = str1;
+	void stringSwap(Strings &str1, Strings &str2){
+		Strings tmp = str1;
 		str1 = str2;
 		str2 = tmp;
 	}
+
+int main(){
+
+	Strings str1 = "string1";
+	Strings str2 = "string2";
 	
+
 	stringSwap(str1, str2);
 		
-	cout << str1 << str2;
-	
+	cout << str1  << "\n" << str2 << "\n";
 }
