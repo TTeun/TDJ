7bf88653aa3bc21cb6304784838694b3a8cdc18d
Dani3981 <danihulzebos@hotmail.com>
2016-10-15 12:23:32 +0200

    ex53

---
+++ week6/53/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+	double value = 12.04;
+
+	cout << setw(15) << value << '\n'									//without any special formatting
+		 << left << setw(15) << value << '\n'							//left aligned 
+		 << right << setw(15) << value << '\n'							//right aligned 
+		 << setprecision(3) << showpoint << setw(15) << value << '\n'	//only the initial 3 digits of value
+		 << setprecision(4) << fixed << setw(15) << value << '\n'		//using 4 numbers behind the decimal point 
+		 << setprecision(2) << setw(15) << value << '\n';	//the original value once again, as in the first line, without any special formatting 
+
+}
