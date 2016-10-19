04505789917fa30f0fcca6e7f3fd89303979e5be
Dani3981 <danihulzebos@hotmail.com>
2016-10-19 21:42:40 +0200

    Update search.h

--- week5/46/search/search.h
+++ week5/46/search/search.h
@@ -15,8 +15,8 @@ class Search
 {
 	std::string d_text;
     std::string (Search::*d_match)(std::string);
-	void (Search::*d_findTarget)(std::string, int);
-	void (Search::*d_showNrs)(int);
+	void (Search::*d_findTarget)(std::string, size_t);
+	void (Search::*d_showNrs)(size_t);
 	void (Search::*d_showLine)(std::string);
 	
 	public:
@@ -27,12 +27,12 @@ class Search
 		void arguments(int argc, char **argv);
         std::string match(std::string);
 		std::string allCasing(std::string);
-		void withBlanks(std::string, int);
-		void withoutBlanks(std::string, int);
-		void showNrs(int);
+		void withBlanks(std::string, size_t);
+		void withoutBlanks(std::string, size_t);
+		void showNrs(size_t);
 		void showLine(std::string);
 		void noOp(std::string);
-		void noOp(int);
+		void noOp(size_t);
 };
         
 #endif
