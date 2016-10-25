5e036641cddbe96de598288a1bc8530ed04e53cc
TTeun <teunverstraaten@gmail.com>
2016-10-25 12:40:10 +0200

    assaas

--- week6/54/54.cc
+++ week6/54/54.cc
@@ -13,10 +13,29 @@ enum Mode
     ALL
 };
 
-int main() {
-    ifstream iStrm("/var/log/account/pacct", ios::in | ios::binary);
-    
+/* 
+ * input as :    ./a.out (-a) (path) 
+ * () means argument is optional.
+ */
+
+int main(int argc, char **argv) 
+{
     Mode mode = DEFAULT;
+    string path = "/var/log/account/pacct";
+    if (argc > 1){
+		if (string(argv[1]) = "-a"){
+			mode = ALL;
+			if (argc > 2)
+				path = argv[2];
+		}
+		else
+			path = argv[1];
+	}
+     
+    ifstream iStrm(path, ios::in | ios::binary);
+    if (not iStrm.good())
+		cout << "Error reading specified file" << '\n';
+
     char *memblock = new char[sizeof(acct_v3)];
     string error;
     while (iStrm.read(memblock, sizeof(acct_v3))) {
@@ -32,5 +51,5 @@ int main() {
                  error << '\n';
         }
     }
-
+    delete memblock;
 }
